e19d6f292c5e018a517411a8f312ae171a687b5e
Vinicius Bisineli Gonaçalves <vinicius.goncalves@eleflow.com.br>
2022-07-03 16:31:30 -0300

    atualizando estrutura de pastas, inserindo consumos das duas abas da planilha e bug fiz no compose

--- dags/dag_raizen_ingestion.py
+++ dags/dag_raizen_ingestion.py
@@ -1,7 +1,16 @@
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.decorators import task
 from airflow.operators.dummy import DummyOperator
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+
+url = 'https://github.com/raizen-analytics/data-engineering-test/raw/master/assets/vendas-combustiveis-m3.xls'
+database = "public"
+
+def inseret_data(url, sheet_name, schema, table):
+    from func.xls_to_pg import ExcelToPG
+    return ExcelToPG.get_data(url, sheet_name, schema, table)
 
 with DAG(
     dag_id="Raize_ingestion",
@@ -13,12 +22,23 @@ with DAG(
     start = DummyOperator(
         task_id="Start"
     )
-
+    end = DummyOperator(
+        task_id="End"
+    )
     # sensor_task = FileSensor( 
     #     task_id= "check_file", 
     #     poke_interval= 30, 
     #     filepath= "/opt/airflow/dag/files/vendas-combustiveis-m3.xls" 
     # )
 
+    insert_oil_data = PythonOperator(
+        task_id = "insert_oil_data",
+        python_callable=inseret_data(url, "DPCache_m3", database, "raizen_oil_derivative")
+    )
 
-    start >> sensor_task
+    insert_diesel_data = PythonOperator(
+        task_id = "insert_disel_data",
+        python_callable=inseret_data(url, "DPCache_m3_2", database, "raizen_disel")
+    )   
+
+    start >> insert_diesel_data >> insert_oil_data >> end
---
+++ dags/func/xls_to_pg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from tokenize import String
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+class ExcelToPG():
+
+    def get_data(url: String, sheet_name: String, schema: String, table:String):
+        
+        # r = requests.get(url)
+        # open('tmp_raizen.xls', 'wb').write(r.content)
+
+        # df = pd.read_excel('tmp_raizen.xls', sheet_name = None)
+        # #, sheet_name="DPCache_m3"
+
+        df = pd.read_excel('./dags/files/vendas-combustiveis-m3.xls', sheet_name=sheet_name)
+        print(df.keys())
+
+        #creating database conection
+        engine = create_engine('postgresql+psycopg2://raizen:raizen@postgres-raizen:5432/postgres')
+
+        #inserting data
+        table_data = schema + '.' + table
+        df.to_sql(name=table_data, con=engine, if_exists='replace',index=False)
+
+
