d4513931f2c7cf72d30513b49e749c18211dc6c3
Vinicius Bisineli Gonaçalves <vinicius.goncalves@eleflow.com.br>
2022-07-05 20:26:34 -0300

    inserinro unit estado

--- plugins/sql/insert_oil_trusted_data.sql
+++ plugins/sql/insert_oil_trusted_data.sql
@@ -2,18 +2,18 @@ with my_table as(
 SELECT combustivel, ano, ul.uf as c_uf, regiao, rd.estado, jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez, total
 FROM raizen_oil_derivative rd
 left join uf_lookup ul on ul.estado=rd.estado ),
-jan as (select to_date(concat(ano,'01'),'YYYYMM') as ano_mes,c_uf,combustivel,jan as mes from my_table),
-fev as (select to_date(concat(ano,'02'),'YYYYMM') as ano_mes,c_uf,combustivel,fev as mes from my_table),
-mar as (select to_date(concat(ano,'03'),'YYYYMM') as ano_mes,c_uf,combustivel,mar as mes from my_table),
-abr as (select to_date(concat(ano,'04'),'YYYYMM') as ano_mes,c_uf,combustivel,abr as mes from my_table),
-mai as (select to_date(concat(ano,'05'),'YYYYMM') as ano_mes,c_uf,combustivel,mai as mes from my_table),
-jun as (select to_date(concat(ano,'06'),'YYYYMM') as ano_mes,c_uf,combustivel,jun as mes from my_table),
-jul as (select to_date(concat(ano,'07'),'YYYYMM') as ano_mes,c_uf,combustivel,jul as mes from my_table),
-ago as (select to_date(concat(ano,'08'),'YYYYMM') as ano_mes,c_uf,combustivel,ago as mes from my_table),
-set as (select to_date(concat(ano,'09'),'YYYYMM') as ano_mes,c_uf,combustivel,set as mes from my_table),
-out as (select to_date(concat(ano,'10'),'YYYYMM') as ano_mes,c_uf,combustivel,out as mes from my_table),
-nov as (select to_date(concat(ano,'11'),'YYYYMM') as ano_mes,c_uf,combustivel,nov as mes from my_table),
-dez as (select to_date(concat(ano,'12'),'YYYYMM') as ano_mes,c_uf,combustivel,dez as mes from my_table),
+jan as (select to_date(concat(ano,'01'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,jan as mes from my_table),
+fev as (select to_date(concat(ano,'02'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,fev as mes from my_table),
+mar as (select to_date(concat(ano,'03'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,mar as mes from my_table),
+abr as (select to_date(concat(ano,'04'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,abr as mes from my_table),
+mai as (select to_date(concat(ano,'05'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,mai as mes from my_table),
+jun as (select to_date(concat(ano,'06'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,jun as mes from my_table),
+jul as (select to_date(concat(ano,'07'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,jul as mes from my_table),
+ago as (select to_date(concat(ano,'08'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,ago as mes from my_table),
+set as (select to_date(concat(ano,'09'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,set as mes from my_table),
+out as (select to_date(concat(ano,'10'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,out as mes from my_table),
+nov as (select to_date(concat(ano,'11'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,nov as mes from my_table),
+dez as (select to_date(concat(ano,'12'),'YYYYMM') as ano_mes,c_uf,combustivel,estado,dez as mes from my_table),
 union_table as (
 select * from jan
 union all
